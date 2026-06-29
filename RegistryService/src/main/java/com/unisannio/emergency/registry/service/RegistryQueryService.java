61bdcb70510ec8b6c02de4b2c7fb4c90b463e862
Carlo <c.mauriello1@studenti.unisannio.it>
2026-06-29 22:27:25 +0200

    Service implementato con EntityManager e JPQL per riferimento didattico

---
+++ RegistryService/src/main/java/com/unisannio/emergency/registry/service/RegistryQueryService.java
@@ -0,0 +1,35 @@
+package com.unisannio.emergency.registry.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.unisannio.emergency.registry.persistance.ServiceInstance;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+//JPA “manuale” + JPQL (EntityManager)
+@Service
+public class RegistryQueryService {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public RegistryQueryService() {}
+
+    @Transactional(readOnly = true)
+    public List<ServiceInstance> getServiceByCapability(String capabilityName) {
+
+    return em.createQuery(
+        "SELECT DISTINCT si " +
+        "FROM ServiceInstance si " +
+        "JOIN si.capabilities c " +
+        "WHERE c.name = :name",
+        ServiceInstance.class
+    )
+    .setParameter("name", capabilityName)
+    .getResultList();
+    }
+}
