2c9df5417b2b28ea36b3e8188d7f3a310e9fbadb
Carlo <c.mauriello1@studenti.unisannio.it>
2026-07-09 16:23:43 +0200

    First commit for the orchestrator service

---
+++ emergency-orchestrator/emergency-orchestrator/src/main/java/com/unisannio/emergency/orchestrator/emergency_orchestrator/EmergencyOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.unisannio.emergency.orchestrator.emergency_orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyOrchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyOrchestratorApplication.class, args);
+	}
+
+}
