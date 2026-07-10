efc26bc8ba605a9bef022477278924dfc9aee593
Carlo <c.mauriello1@studenti.unisannio.it>
2026-07-10 12:54:18 +0200

    Refactor ClassEmergencyWorkflow to remove WorkflowResolver dependency and update execute method logic; add CapabilityResolver class for handling service capabilities.

--- emergency-orchestrator/src/main/java/com/unisannio/emergency/orchestrator/emergency_orchestrator/controller/ClassEmergencyWorkflow.java
+++ emergency-orchestrator/src/main/java/com/unisannio/emergency/orchestrator/emergency_orchestrator/controller/ClassEmergencyWorkflow.java
@@ -6,28 +6,28 @@ import org.springframework.stereotype.Controller;
 
 import com.unisannio.emergency.orchestrator.emergency_orchestrator.model.EmergencyEvent;
 import com.unisannio.emergency.orchestrator.emergency_orchestrator.model.EmergencyService;
-import com.unisannio.emergency.orchestrator.emergency_orchestrator.model.ServiceResolution;
-import com.unisannio.emergency.orchestrator.emergency_orchestrator.service.WorkflowResolver;
+
+
 import com.unisannio.emergency.orchestrator.emergency_orchestrator.util.RankingStrategy;
 
 @Controller
 public class ClassEmergencyWorkflow {
    
     private RankingStrategy strategy;
-    private WorkflowResolver resolver;
+  
 
-    public ClassEmergencyWorkflow(RankingStrategy strategy, WorkflowResolver resolver) {
-        this.strategy = strategy;
-        this.resolver = resolver;
+    public ClassEmergencyWorkflow(RankingStrategy strategy) {
+      this.strategy = strategy;
+        
     }
 
     
     public final void execute(EmergencyEvent event){
         validate(event); //
        
-        ServiceResolution resolution = resolver.resolve(event); //Qua prende il workflow (Factory o db)
+        /*ServiceResolution resolution = resolver.resolve(event); //Qua prende il workflow (Factory o db)
         EmergencyService chosen = strategy.select(workflow.services());   //Qua strategy per prendere dal registry i servizi disponibili (il registry altra classe che gestisce hashmap e Db)
-        callService(chosen); //Chiamata HTTP
+        Scheduler callServices(chosenQueue, chosenParallel); //Chiamata HTTP
         /*
         try {
           callService(chosen, event); //Chiamata HTTP
@@ -36,8 +36,8 @@ public class ClassEmergencyWorkflow {
              ExplicitFallbackHandling();
         }
         */
-        Evacuate(Workflow workflow);
-        postProcess(event); // Esempio logging
+        /*Evacuate(Workflow workflow);
+        postProcess(event); // Esempio logging/ */
     }
     
     // STEP FISSI
