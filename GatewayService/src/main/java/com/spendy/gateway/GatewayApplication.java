d659fd441e5375d190f6f77085b10f8c637e7a54
gius03 <giuseppe100703@gmail.com>
2026-09-07 21:04:06 +0200

    feat: update dependencies and adjust Spring Cloud and Camunda versions

--- GatewayService/src/main/java/com/spendy/gateway/GatewayApplication.java
+++ GatewayService/src/main/java/com/spendy/gateway/GatewayApplication.java
@@ -1,11 +1,11 @@
 package com.spendy.gateway;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
 @RestController
 @SpringBootApplication(scanBasePackages = "com.spendy.gateway")
 public class GatewayApplication {
