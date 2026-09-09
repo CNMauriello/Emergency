8858f1b26499ff982dfa386bbfd278f43024c8c8
gius03 <giuseppe100703@gmail.com>
2026-09-09 18:16:08 +0200

    feat: update docker-compose and BPMN files for service integration and port exposure

--- OrchestratorService/src/main/java/com/unisannio/emergency/orchestrator/EmergencyOrchestratorApplication.java
+++ OrchestratorService/src/main/java/com/unisannio/emergency/orchestrator/EmergencyOrchestratorApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Deployment(
-        resources = "classpath:Emergency_Response_Plan_Orchestrator.bpmn"
-)
+@Deployment(resources = "file:/app/bpmns/*.bpmn")
 public class EmergencyOrchestratorApplication {
 
     public static void main(String[] args) {
