a576b956f808fd54b7d9791fb087c9259e6e2b34
gius03 <giuseppe100703@gmail.com>
2026-09-09 16:18:39 +0200

    feat: update Dockerfile and startup script for Zeebe version upgrade and initialization check

--- start-bundle.sh
+++ start-bundle.sh
@@ -3,8 +3,12 @@ echo "Starting Camunda Zeebe Engine..."
 export ZEEBE_BROKER_NETWORK_HOST=0.0.0.0
 /app/zeebe/bin/broker &
 
-echo "Waiting for Zeebe to initialize (15s)..."
-sleep 15
+echo "Waiting for Zeebe to initialize..."
+while ! curl -s http://localhost:8080 > /dev/null; do
+  echo "Waiting for Zeebe on port 8080..."
+  sleep 3
+done
+echo "Zeebe is up!"
 
 echo "Starting BinderService..."
 java -jar /app/binder.jar &
