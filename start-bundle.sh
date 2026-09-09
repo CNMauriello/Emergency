7f03f4283145b46453f525c57951b922e4326828
gius03 <giuseppe100703@gmail.com>
2026-09-09 17:16:22 +0200

    feat: enhance docker-compose and startup script with health checks and elasticsearch integration

--- start-bundle.sh
+++ start-bundle.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 echo "Starting Camunda Zeebe Engine..."
+
+sed -i 's/localhost:9200/elasticsearch:9200/g' /app/zeebe/config/application.yaml
+sed -i 's/unprotectedApi: false/unprotectedApi: true/g' /app/zeebe/config/application.yaml
+
 export ZEEBE_BROKER_NETWORK_HOST=0.0.0.0
 /app/zeebe/bin/broker &
 
